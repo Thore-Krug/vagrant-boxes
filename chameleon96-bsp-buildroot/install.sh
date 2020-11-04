a5c514d8aa13d09b81d3b6a06ad5a7ccf4b8aeec
Thore Krug <opensource@thorekrug.de>
2020-11-04 19:18:22 +0100

    Minor change in installation provision script
    Signed off by: Thore Krug opensource[at]thorekrug.de

--- chameleon96-bsp-buildroot/install.sh
+++ chameleon96-bsp-buildroot/install.sh
@@ -2,11 +2,11 @@
 
 echo " Setting up the Development enviroment "
 
-cat  https://github.com/Thore-Krug/vagrant-boxes/blob/master/chameleon96-bsp-buildroot/icon.txt
+curl  https://github.com/Thore-Krug/vagrant-boxes/blob/master/chameleon96-bsp-buildroot/icon.txt
 
 sudo apt-get update
 
-xargs -a https://github.com/Thore-Krug/vagrant-boxes/blob/master/chameleon96-bsp-buildroot/packages.txt sudo apt-get install
+curl https://github.com/Thore-Krug/vagrant-boxes/blob/master/chameleon96-bsp-buildroot/packages.txt | xargs sudo apt-get install
  
 git clone https://github.com/Chameleon960/BSP-buildroot
 
